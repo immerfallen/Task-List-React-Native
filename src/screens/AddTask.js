f3bf6d661bf679c6e9be030d07f1b0f1deaca86d
Maro de Melo <marodemelo10@gmail.com>
2021-05-06 12:19:55 -0300

    Ajustado calendario para inserir data da tarefa

--- src/screens/AddTask.js
+++ src/screens/AddTask.js
@@ -1,15 +1,37 @@
 import React, {Component} from 'react'
-import {Modal, View, StyleSheet, TouchableWithoutFeedback, Text, TextInput, TouchableOpacity, Button} from 'react-native'
+import {Modal, View, StyleSheet, TouchableWithoutFeedback, Text, TextInput, TouchableOpacity, Platform} from 'react-native'
 
+import DateTimePicker from '@react-native-community/datetimepicker'
 import commonStyles from '../commonStyles'
+import moment from 'moment'
 
 
-const initialState ={ desc: ''}
+const initialState ={ desc: '', date: new Date(), showDatePicker: false}
 
 export default class AddTask extends Component {
 
     state ={ 
         ...initialState
+    }
+
+    getDatePicker = () =>{
+        let datePicker = <DateTimePicker value={this.state.date} onChange={(_, date) => this.setState({date:date, showDatePicker: false})} mode='date' />
+
+        const dateString = moment(this.state.date).format('dddd, D [de] MMMM [de] YYYY')
+
+        if (Platform.OS==='android') {
+            datePicker = (
+                <View>
+                    <TouchableOpacity onPress={() => this.setState({showDatePicker: true})}>
+                        <Text style={styles.date}>
+                            {dateString}
+                        </Text>
+                    </TouchableOpacity>
+                    {this.state.showDatePicker && datePicker}
+                </View>
+            )
+        }
+        return datePicker
     }
 
     render() {
@@ -23,6 +45,7 @@ export default class AddTask extends Component {
                 <View style={styles.container}>
                     <Text style={styles.header}>Nova Tarefa</Text>
                     <TextInput style={styles.input} placeholder='Informe a descrição' onChangeText={desc => this.setState({desc : desc})} value={this.state.desc}/>
+                    {this.getDatePicker()}
                     <View style={styles.buttons}>
                         <TouchableOpacity onPress={this.props.onCancel}>
                            <Text style={styles.button}>Cancelar</Text>
@@ -77,5 +100,12 @@ const styles = StyleSheet.create({
         margin: 20,
         marginRight: 30,
         color: commonStyles.colors.today,
+    },
+    date:{
+        fontFamily: commonStyles.fontFamily,
+        fontSize: 20,
+        marginLeft: 15,
+
+
     }
 })
--- src/screens/TaskList.js
+++ src/screens/TaskList.js
@@ -14,7 +14,7 @@ export default class TaskList extends Component {
 
     state ={
         showDoneTasks: true,
-        showAddTask: true,
+        showAddTask: false,
         visibleTasks: [],
         tasks: [{
             id: Math.random(),
@@ -90,7 +90,10 @@ export default class TaskList extends Component {
                    <FlatList data={this.state.visibleTasks}
                    keyExtractor={item => `${item.id}`}
                    renderItem= {({item}) => <Task {...item}  toggleTask={this.toggleTask}/>} />
-                </View>                
+                </View>   
+                <TouchableOpacity style={styles.addButton} activeOpacity={0.7} onPress={()=>this.setState({showAddTask: true})}>
+                    <Icon name='plus' size={20} color={commonStyles.colors.secondary}/>    
+                </TouchableOpacity>             
             </View>
         )
     }
@@ -132,5 +135,16 @@ const styles = StyleSheet.create({
         justifyContent: 'flex-end',
         marginTop: Platform.OS === 'ios' ? 40 : 10
 
+    },
+    addButton: {
+        position: 'absolute',
+        right:30,
+        bottom: 30,
+        width: 50,
+        height: 50,
+        borderRadius: 25,
+        backgroundColor: commonStyles.colors.today,
+        justifyContent: 'center',
+        alignItems: 'center'
     }
 })
