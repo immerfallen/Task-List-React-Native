cf36971edcbca4a7f148df60c7c393a41fc19fbc
Maro de Melo <marodemelo10@gmail.com>
2021-07-18 15:33:14 -0300

    melhorada mensagens de erro

--- src/common.js
+++ src/common.js
@@ -3,7 +3,12 @@ import {Alert, Platform} from 'react-native'
 const server = Platform.OS === 'ios' ? 'http://localhost:3000' : 'http://192.168.15.102:3000'
 
 function showError(err) {
+    if(err.response && err.response.data){
+    Alert.alert('Ops, ocorreu um problema', `Mensagem: ${err.response.data}`)
+    }
+    else {
     Alert.alert('Ops, ocorreu um problema', `Mensagem: ${err}`)
+    }
 }
 
 function showSuccess(msg) {
